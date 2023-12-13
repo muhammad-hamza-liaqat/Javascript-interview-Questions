c4dbd271bdc1818efe37377a8f83a9be3566018d
Muhammad Hamza <mh408800@gmail.com>
2023-12-13 11:22:12 +0500

    hoisting

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Javascript-interview-Questions
 a list of javascript interview questions
+
+<!-- primitive types and non promivite types  -->
+
+ primitives types are the pre defined data types in javascript such as int bigInt boolean non-primitive are the combination of different data types
+ var object1={
+     let x=10
+     let y = "hamza" 
+ };
--- questions.js
+++ questions.js
@@ -1,6 +1,21 @@
-// primitive types and non promivite types 
-// primitives types are the pre defined data types in javascript such as int bigInt boolean non-primitive are the combination of different data types
-// var object1={
-    // let x=10
-    // let y = "hamza" 
-// };
+// hoisting
+
+hoistedVariable = 10;
+console.log(hoistedVariable);
+var hoistedVariable;
+
+hoistedFunction();
+
+function hoistedFunction(){
+    console.log("called first declared after")
+}
+
+// accNumber = "01005639601";
+console.log("account Number: ",accNumber);
+// declared the variable but not assigned to the value
+var accNumber;
+
+name= "Muhammad Hamza Liaqat"
+console.log(name);
+// let name; // reference error will be thrown
+
