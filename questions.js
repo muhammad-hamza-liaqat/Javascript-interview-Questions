e1fffd73fcd72600b356e413d92f5611f450edf0
Muhammad Hamza <mh408800@gmail.com>
2023-12-13 11:14:13 +0500

    questions

---
+++ questions.js
@@ -0,0 +1,6 @@
+// primitive types and non promivite types 
+// primitives types are the pre defined data types in javascript such as int bigInt boolean non-primitive are the combination of different data types
+// var object1={
+    // let x=10
+    // let y = "hamza" 
+// };
